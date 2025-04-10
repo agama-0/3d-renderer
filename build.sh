072c3bc84a2bdc06ff371c74390d9ea65e071cc5
Harshit Shivhare <harshit.owo@gmail.com>
2025-04-10 09:18:35 +0530

    fixed build files

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ mkdir -p .out
 clang -c ./external/glad/src/glad.c -I./external/glad/include
 # clang -c src/*.c -I./external/glad/include -I./external/glfw/include
 clang++ -c src/*.cpp -I./external/glad/include -I./external/glfw/include
+clang++ -o ./.out/renderer.out ./*.o -L./external/glfw/src -lglfw3
 rm ./*.o
